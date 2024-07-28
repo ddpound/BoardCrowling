9c8a3de58439ab1c4fd2837eb8205e7d8e4041ee
dpound@naver.com <dpound@naver.com>
2024-07-29 05:35:16 +0900

    beta 버전 구현 완료, 프로젝트에 당장 적용 가능하도록 하드코딩되어있음. 추후에 유연하게 작동하도록 수정 요망

--- src/main/java/com/nhcb/Main.java
+++ src/main/java/com/nhcb/Main.java
@@ -1,23 +1,41 @@
 package com.nhcb;
 
 import com.nhcb.manager.DatabaseManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.yaml.snakeyaml.Yaml;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("프로그램 시작");
-        String url = "https://www.ispf.or.kr/notice/94";
+        BoardCrowling("https://www.ispf.or.kr/notice/110");
+
+//
+//        for (int i = 25; i < 112; i++) {
+//            BoardCrowling("https://www.ispf.or.kr/notice/" + i);
+//        }
+    }
+
+    public static void BoardCrowling(String url) {
+
         DatabaseManager dbManager = new DatabaseManager();
         boolean directInsert = true; // true면 직접 삽입, false면 SQL 파일로 내보내기
         String outputFilePath = "insert_statements.sql";
@@ -25,57 +43,132 @@ public class Main {
 
         try {
             Document doc = Jsoup.connect(url).get();
-            Elements posts = doc.select("#bo_v"); // 게시물 리스트 선택자
 
-            System.out.println("크롤링 시작");
+            if(isPageNotFound(doc)){
+                System.out.println("글이 없습니다.");
+                System.out.println("없는 글 url : " + url);
+            }else{
+                Elements posts = doc.select("#bo_v"); // 게시물 리스트 선택자
 
-            for (Element post : posts) {
-                count++;
-                String category = post.select(".bview_cate").text();
-                String title = post.select(".bo_v_tit").text();
-                Element contentElement = post.select("#bo_v_con").first();
-                Elements writerInfoElements = post.select("div.writer_info .txt");
+                System.out.println("크롤링 시작");
 
-                // IMG 요소 처리
-                if (contentElement != null) {
-                    Elements imgElements = contentElement.select("img");
-                    for (Element imgElement : imgElements) {
-                        String imgUrl = imgElement.attr("src");
-                        String base64Image = convertImageToBase64("https://www.ispf.or.kr/data/editor/",imgUrl);
-                        imgElement.attr("src", "data:image/png;base64," + base64Image);
+                for (Element post : posts) {
+                    count++;
+                    String category = post.select(".bview_cate").text();
+                    String title = post.select(".bo_v_tit").text();
+                    Element contentElement = post.select("#bo_v_con").first();
+                    Elements writerInfoElements = post.select("div.writer_info .txt");
+
+                    // '#bo_v_con' 요소의 자식들만 추출
+                    Elements childElements = contentElement.children();
+
+                    // 또는 자식 요소들 HTML을 하나의 문자열로 연결
+                    StringBuilder contentHtml = new StringBuilder();
+                    for (Element child : childElements) {
+                        contentHtml.append(child.outerHtml());
                     }
+
+                    // IMG 요소 처리
+                    if (contentElement != null) {
+                        Elements imgElements = contentElement.select("img");
+                        for (Element imgElement : imgElements) {
+                            String imgUrl = imgElement.attr("src");
+                            String base64Image = convertImageToBase64("https://www.ispf.or.kr/data/editor/",imgUrl);
+                            imgElement.attr("src", "data:image/png;base64," + base64Image);
+                        }
+                    }
+                    String content = contentElement != null ? contentElement.html() : "";
+                    String escapedTitle = StringEscapeUtils.escapeHtml4(title); // HTML 내용 이스케이프
+                    String escapedContent = StringEscapeUtils.escapeHtml4(String.valueOf(contentHtml)); // HTML 내용 이스케이프
+
+                    // 날짜 요소를 가져옴
+                    String date = writerInfoElements.get(1).text();
+                    int inqCnt = Integer.parseInt(writerInfoElements.get(2).text()); // 조회수
+
+                    /**
+                     * 파일 리스트 추출
+                     * fileslist class 중에
+                     * https://www.ispf.or.kr/ 로 시작하는 링크가 있으면 다운로드 파일 생성해주자
+                     * file root 경로에서 YYYYMMDD 형식 안에 파일이 들어감
+                     * */
+                    // 'fileslist' 클래스명을 가진 요소 찾기
+                    Elements filesListElements = doc.getElementsByClass("fileslist");
+                    List<String> fileList = new ArrayList<>(); // 다운로드 리스트
+                    List<String> relatedLinks = new ArrayList<>(); // 관련링크
+
+                    // 각 'fileslist' 요소의 자식 'a' 태그를 찾고 href 추출
+                    for (Element filesListElement : filesListElements) {
+                        Elements anchorElements = filesListElement.select("a");
+                        for (Element anchor : anchorElements) {
+                            String href = anchor.attr("href"); // href 속성 추출
+                            if(href.startsWith("https://www.ispf.or.kr/bbs/download")) fileList.add(href); // href를 리스트에 추가
+                            else relatedLinks.add(href); // 관련링크
+                        }
+                    }
+
+                    String fileGroupId = null;
+
+                    // 추출한 href 출력
+                    if(!fileList.isEmpty()) {
+                        System.out.println("파일 다운로드 감지");
+                        fileGroupId = DatabaseManager.getSeqFileNumber();
+                        DatabaseManager.downloadAllFiles(url,fileGroupId);
+                    }
+
+
+                    if (directInsert) {
+                        String categoryNumber = "";
+                        if(category.equals("공고")) categoryNumber = "2024030001";
+                        if(category.equals("알림")) categoryNumber = "2024030002";
+                        if(category.equals("보도자료")) categoryNumber = "2024030003";
+
+                        // 데이터베이스에 삽입
+                        dbManager.insertPost("pts_cmmnty_detail_mstr",
+                                categoryNumber,
+                                escapedTitle,
+                                date,
+                                content,
+                                relatedLinks,
+                                fileList,
+                                fileGroupId,
+                                inqCnt);
+                    } else {
+                        // SQL INSERT 문 생성 및 파일로 내보내기
+                        //String insertSQL = dbManager.generateInsertSQL(category, title, date, content);
+                        //dbManager.exportInsertSQLToFile(outputFilePath, insertSQL);
+                    }
+
+                    System.out.println("Category: " + category);
+                    System.out.println("Title: " + title);
+                    System.out.println("Date: " + date);
+                    //System.out.println("Content: " + escapedContent);
+                    System.out.println("count " + count);
+                    System.out.println("===================================");
                 }
-                String content = contentElement != null ? contentElement.html() : "";
-                String escapedContent = StringEscapeUtils.escapeHtml4(content); // HTML 내용 이스케이프
-
-                // 날짜 요소를 가져옴
-                String date = writerInfoElements.get(1).text();;
-
-                if (directInsert) {
-                    String categoryNumber = "";
-                    if(category.equals("공고")) categoryNumber = "2024030001";
-                    if(category.equals("알림")) categoryNumber = "2024030002";
-                    if(category.equals("보도자료")) categoryNumber = "2024030003";
-                    // 데이터베이스에 삽입
-                    dbManager.insertPost("pts_cmmnty_detail_mstr", categoryNumber, title, date, content);
-                } else {
-                    // SQL INSERT 문 생성 및 파일로 내보내기
-                    //String insertSQL = dbManager.generateInsertSQL(category, title, date, content);
-                    //dbManager.exportInsertSQLToFile(outputFilePath, insertSQL);
-                }
-
-                System.out.println("Category: " + category);
-                System.out.println("Title: " + title);
-                System.out.println("Date: " + date);
-                System.out.println("Content: " + escapedContent);
-                System.out.println("count " + count);
-                System.out.println("===================================");
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static boolean isPageNotFound(Document doc) {
+        // 페이지에 "글이 존재하지 않습니다" 메시지가 포함된 경우 처리
+        Elements scriptElements = doc.select("script");
+        for (Element script : scriptElements) {
+            if (script.toString().contains("alert(\"글이 존재하지 않습니다.\"")) {
+                return true;
+            }
+        }
+        Elements noScriptElements = doc.select("noscript");
+        for (Element noScript : noScriptElements) {
+            if (noScript.toString().contains("글이 존재하지 않습니다.")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     private static String convertImageToBase64(String baseUrl, String imageUrl) {
         try {
--- src/main/java/com/nhcb/manager/DatabaseManager.java
+++ src/main/java/com/nhcb/manager/DatabaseManager.java
@@ -1,37 +1,280 @@
 package com.nhcb.manager;
 
 import com.nhcb.config.YamlConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class DatabaseManager {
 
-    private Connection connection;
+    private static Connection connection;
+
+    private static String saveFilePath;
+
+    private static String saveDir;
+
+    // WebDriver 초기화
+    private static WebDriver driver;
 
     public DatabaseManager() {
         YamlConfig config = new YamlConfig();
         try {
             connection = DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
+            Yaml yaml = new Yaml();
+            try (InputStream in = getClass().getResourceAsStream("/application.yml")) {
+                Map<String, Object> configData = yaml.load(in);
+                Map<String, String> fileConfig = (Map<String, String>) configData.get("file");
+                this.saveFilePath = fileConfig.get("fileloc");
+                this.saveDir = fileConfig.get("saveDir");
+                System.out.println("file config check : " + saveFilePath);
+                System.out.println("file config check : " + saveDir);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void insertPost(String tableName, String cmmntyId, String title, String date, String content) {
+    public void insertPost(String tableName,
+                           String cmmntyId,
+                           String title,
+                           String date,
+                           String content,
+                           List<String> linkList,
+                           List<String> fileList,
+                           String fileGroupId,
+                           int inqCnt) {
+        // 추출
+        String datePart = date.substring(0, 8); // "24-04-23"
+
+        // 변환을 위한 포맷터 정의
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yy-MM-dd");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        // LocalDate 객체로 변환
+        LocalDate changeDate = LocalDate.parse(datePart, inputFormatter);
+
+        // 원하는 형식으로 변환
+        String formattedDate = changeDate.format(outputFormatter);
+
+        // 현재 날짜와 1년 뒤 날짜를 LocalDate 객체로 얻기
+        LocalDate today = LocalDate.now();
+        LocalDate oneYearLater = today.plusYears(1);
+
+        // YYYYMMDD 형식으로 변환
+        String pstgBgngDate = today.format(outputFormatter);
+        String pstgEndDate = oneYearLater.format(outputFormatter);
+
+        String link1 = null;
+        String link2 = null;
+
+        // 링크 리스트 추출
+        if(linkList != null && !linkList.isEmpty()){
+            link1 = linkList.get(0) != null ? linkList.get(0) : null;
+            if(linkList.size() > 1) link2 = linkList.get(1) != null ? linkList.get(0) : null;
+        }
+
+        // 파일이 있다는 의미니 파일 업로드진행
+        if(fileList != null && !fileList.isEmpty()){
+
+        }
+
         String sql = "INSERT INTO " + tableName
-                + " (cmmnty_id , cnn_no , ntt_sbjt, date, ntt_cntn, pstg_trg, rgst_id, reg_dt, updt_dt, del_yn) "
-                + "VALUES (?, COALESCE((SELECT MAX(ntt_no) + 1 FROM pts_cmmnty_detail_mstr), 1) ,?, ?, ?, 1, 'SYSTEM', now(), now(), '0')";
+                + " (cmmnty_id , ntt_no , ntt_sbjt, reg_ymd, ntt_cntn, pstg_trgt, ntcr_id ,rgst_id,updn_id , reg_dt, updt_dt, del_yn, pstg_bgng_date, pstg_end_date, link_cntn_1, link_cntn_2, atch_file_group_id, ntcr_nm, inq_cnt) "
+                + "VALUES (?, COALESCE((SELECT MAX(ntt_no) + 1 FROM pts_cmmnty_detail_mstr), 1) ,?, ?, ?, 1, 'SYSTEM','SYSTEM', 'SYSTEM', now(), now(), '0', ?, ?, ? ,? , ?, 'SYSTEM', ?)";
 
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setString(1, cmmntyId);
             pstmt.setString(2, title);
-            pstmt.setString(3, date);
+            pstmt.setString(3, formattedDate);
             pstmt.setString(4, content);
+            pstmt.setString(5, pstgBgngDate);
+            pstmt.setString(6, pstgEndDate);
+            pstmt.setString(7, link1);
+            pstmt.setString(8, link2);
+            pstmt.setString(9, fileGroupId);
+            pstmt.setInt(10, inqCnt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** 파일 그룹 아이디 시퀸스를 따옴 */
+    public static String getSeqFileNumber() {
+        String seqFileNumber = null;
+        String sql = "SELECT TO_CHAR(NOW(), 'YYYYMMDD') || LPAD((SELECT NEXTVAL('PTS_SEQ_FILE_ID'))::TEXT, 3, '0') AS file_id";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                seqFileNumber = rs.getString("file_id");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return seqFileNumber;
+    }
+
+    public static void downloadAllFiles(String pageUrl,String fileGroupId) throws IOException {
+        WebDriver driver = null;
+        try {
+            // WebDriverManager를 사용하여 ChromeDriver 설정
+            WebDriverManager.chromedriver().setup();
+
+            // Chrome 옵션 설정
+            ChromeOptions options = new ChromeOptions();
+            // 다운로드 경로 설정
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("download.default_directory", saveDir+saveFilePath);
+            prefs.put("download.prompt_for_download", false);
+            prefs.put("download.directory_upgrade", true);
+            prefs.put("safebrowsing.enabled", true);
+            options.setExperimentalOption("prefs", prefs);
+
+            // 기타 Chrome 옵션 설정
+            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless"); // headless 모드 (브라우저 UI 없이 실행)
+
+            driver = new ChromeDriver(options);
+
+            // 페이지로 이동
+            driver.get(pageUrl);
+
+            // 다운로드 링크들 찾기
+            List<WebElement> downloadLinks = driver.findElements(By.cssSelector("a.view_file_download"));
+
+            // 파일 그룹 채번
+            int sersNo = 1;
+            // 각 링크 클릭하여 다운로드
+            for (WebElement downloadLink : downloadLinks) {
+
+                String href = downloadLink.getAttribute("href");
+                System.out.println("다운로드 링크 클릭: " + href);
+                downloadLink.click();
+
+                String originalFileName = downloadLink.getText();
+                // 새로운 파일 이름 생성 (UUID 사용)
+
+                // 파일 다운로드 대기 (필요에 따라 시간 조정)
+                Thread.sleep(1000);
+
+                // 다운로드된 파일 확인
+                File downloadedFile = getLatestFileFromDir(saveDir+saveFilePath);
+                if (downloadedFile != null) {
+                    // UUID를 사용하여 새 파일 이름 생성
+                    String newFileName = generateRandomFileName(originalFileName);
+                    File renamedFile = new File(saveDir+saveFilePath + newFileName);
+
+                    // 파일 이름 변경
+                    if (downloadedFile.renameTo(renamedFile)) {
+                        System.out.println("원본 파일 이름 1 : " + originalFileName);
+                        System.out.println("파일 다운로드가 완료되었습니다 2 : " + renamedFile.getName());
+
+                        // 이름 변경에 성공했으면 DB에 저장하도록하자
+                        insertFileInfomation(fileGroupId,sersNo,renamedFile.getName(),originalFileName,"SYSTEM","SYSTEM");
+
+                        sersNo++; // 저장후 채번 증가
+                    } else {
+                        System.out.println("파일 이름 변경에 실패했습니다.");
+                    }
+                } else {
+                    System.out.println("파일 다운로드에 실패했습니다.");
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt(); // InterruptedException 발생 시 현재 스레드 상태를 복구
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+
+
+    // 다운로드된 파일 중 가장 최근 파일을 반환하는 메서드
+    private static File getLatestFileFromDir(String dirPath) {
+        Path dir = Paths.get(dirPath);
+        try {
+            return Files.list(dir)
+                    .filter(f -> !Files.isDirectory(f))
+                    .max((f1, f2) -> Long.compare(f1.toFile().lastModified(), f2.toFile().lastModified()))
+                    .map(Path::toFile)
+                    .orElse(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 원래 파일 이름을 기반으로 랜덤한 파일 이름 생성
+    private static String generateRandomFileName(String originalFileName) {
+        // 파일 확장자 추출
+        String extension = "";
+        int dotIndex = originalFileName.lastIndexOf('.');
+        if (dotIndex > 0) {
+            extension = originalFileName.substring(dotIndex); // 파일 확장자
+        }
+
+        // 랜덤 UUID 생성 및 확장자 추가
+        String randomFileName = UUID.randomUUID().toString() + extension;
+        return randomFileName;
+    }
+
+    /**
+     * 파일 정보 저장하는 곳
+     * file information data save
+     * */
+    public static void insertFileInfomation(String ATCH_FILE_GROUP_ID,
+                                            int SERS_NO,
+                                            String STRE_FILE_NM,
+                                            String ORGINL_FILE_NM,
+                                            String RGST_ID,
+                                            String UPDN_ID){
+
+        String pathInfo = saveFilePath;
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        pathInfo = pathInfo + now.format(formatter);
+
+
+        String sql = "INSERT INTO PTS_FILE_MNG_MSTR"
+                + " (ATCH_FILE_GROUP_ID , SERS_NO , FILE_LOC, STRE_FILE_NM, ORGINL_FILE_NM, REG_DT, RGST_ID ,UPDT_DT,UPDN_ID) "
+                + "VALUES (?,?,?,?,?,now(),?,now(),?)";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, ATCH_FILE_GROUP_ID);
+            pstmt.setInt(2, SERS_NO);
+            pstmt.setString(3, pathInfo);
+            pstmt.setString(4, STRE_FILE_NM);
+            pstmt.setString(5, ORGINL_FILE_NM);
+            pstmt.setString(6, RGST_ID);
+            pstmt.setString(7, UPDN_ID);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
